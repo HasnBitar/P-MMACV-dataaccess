7db1b3057813c2f9cc8e072543dc13b2e2853402
Hasn Bitar <h.9311@hotmail.com>
2025-10-19 13:06:59 +0200

    feat(P-MMACV-dataaccess): create the configuration for dataaccess in application.properties

--- src/main/java/com/dataaccess/P_MMACV_dataaccess/entitys/mysql/PersonalInfos.java
+++ src/main/java/com/dataaccess/P_MMACV_dataaccess/entitys/mysql/PersonalInfos.java
@@ -7,23 +7,24 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
-@RequiredArgsConstructor
 @NoArgsConstructor
-@Table(name = "PersonalInfos")
+@AllArgsConstructor
+@Table(name = "Personal_infos")
+@Builder
 public class PersonalInfos {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
-    @Column(nullable = false, name = "firstName")
+    @Column(nullable = false, name = "first_name")
     private String firstName;
-    @Column(nullable = false, name = "lastName")
+    @Column(nullable = false, name = "last_name")
     private String lastName;
     @Column(nullable = false, name = "address")
     private String address;
     @Column(nullable = false, name = "email")
     private String email;
-    @Column(nullable = false, name = "phoneNumber")
+    @Column(nullable = false, name = "phone_number")
     private String phoneNumber;
 
 }
